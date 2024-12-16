12dd1bd80854cf1727735af621f8a718ec899ee2
shakhzodbek1997 <yuldoshov.shakhozd@gmail.com>
2024-12-16 08:19:48 +0100

    Cache then Network with Offline Support

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,3 @@
-
 var deferredPrompt;
 
 if (!window.Promise) {
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -30,6 +30,23 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+function onSaveButtonClicked(event){
+  console.log('Clicked');
+  if('caches' in window){
+    caches.open('user-requested')
+      .then(function(cache){
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
+  }
+}
+
+function clearCards(){
+  while(sharedMomentsArea.hasChildNodes()){
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
+
 function createCard() {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
@@ -40,6 +57,7 @@ function createCard() {
   cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
   cardTitleTextElement.className = 'mdl-card__title-text';
   cardTitleTextElement.textContent = 'San Francisco Trip';
   cardTitle.appendChild(cardTitleTextElement);
@@ -47,15 +65,43 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
+
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get')
+var url = 'https://httpbin.org/get';
+var networkDataReceived = false;
+// for Network
+fetch(url)
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
+    networkDataReceived = true;
+    console.log('From Web', data);
+    clearCards();
     createCard();
   });
+
+  //  For cache
+if('caches' in window){
+  caches.match(url)
+    .then(function(response){
+      if(response){
+        return response.json();
+      }
+    })
+    .then(function(data){
+      console.log('From Cache', data);
+      if(!networkDataReceived){
+        clearCards();
+        createCard();
+      }
+    })
+}
