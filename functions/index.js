fefbb6489765a9ec33de12d4787b55f9d939b017
shakhzodbek1997 <yuldoshov.shakhozd@gmail.com>
2024-12-25 20:58:07 +0100

    Storing to the firebase issue fixed

--- functions/index.js
+++ functions/index.js
@@ -1,112 +1,56 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const cors = require("cors")({ origin: true });
-const webpush = require("web-push");
-const fs = require("fs");
-const UUID = require("uuid-v4");
-const os = require("os");
-const Busboy = require("busboy");
-const path = require("path");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// Initialize Firebase Admin
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
+
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+const serviceAccount = require("./uehsgram-fb-key.json");
+
 admin.initializeApp({
-  credential: admin.credential.cert(require("./uehsgram-fb-key.json")),
-  databaseURL: "https://uehsgram-default-rtdb.europe-west1.firebasedatabase.app/"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://uehsgram-default-rtdb.europe-west1.firebasedatabase.app'
 });
 
-// Cloud function to store post data
-exports.storePostData = functions.https.onRequest((request, response) => {
-  // Enable CORS for handling cross-origin requestsbu
-  cors(request, response, async () => {
-    const uuid = UUID();
-    const busboy = new Busboy({ headers: request.headers });
 
-    let upload;
-    const fields = {};
+exports.storePostData = functions.https.onRequest((req, res) => {   // storePostData - is my API 
+  cors(req, res, async () => { 
+    try{
+      const{id, title, location, image} = req.body;
 
-    // Handle file upload
-    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-      console.log(`File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`);
-      const filepath = path.join(os.tmpdir(), filename);
-      upload = { file: filepath, type: mimetype };
-      file.pipe(fs.createWriteStream(filepath));
-    });
-
-    // Handle other form fields
-    busboy.on("field", (fieldname, val) => {
-      fields[fieldname] = val;
-    });
-
-    // After upload finishes
-    busboy.on("finish", async () => {
-      try {
-        // Upload the file to Firebase Storage
-        const bucket = admin.storage().bucket();
-        const file = bucket.file(upload.file);
-
-        // Upload the file to Firebase Storage
-        await bucket.upload(upload.file, {
-          destination: file,
-          metadata: {
-            contentType: upload.type,
-            metadata: {
-              firebaseStorageDownloadTokens: uuid,
-            },
-          },
-        });
-
-        // Construct the file's public URL
-        const fileUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(file.name)}?alt=media&token=${uuid}`;
-
-        // Store post data in the Firebase Realtime Database
-        await admin.database().ref("posts").push({
-          title: fields.title,
-          location: fields.location,
-          rawLocation: {
-            lat: fields.rawLocationLat,
-            lng: fields.rawLocationLng,
-          },
-          image: fileUrl,
-        });
-
-        // Send push notifications
-        webpush.setVapidDetails(
-          "mailto:yuldoshov.shakhzod@gmail.com",
-          "BPELivJlGUezg5W3ZgM9s5k6K3-PTYPNkSNJstNIwM71AAzxHG9amaaLcAUKHLhAv0ay4ZG3cUEQfrEPpPxrIm4",
-          "Aokjsli1EL4re5poDZzZFiiA-h_wDDN0obhAUTc1LqE"
-        );
-
-        const subscriptionsSnapshot = await admin.database().ref("subscriptions").once("value");
-        const subscriptions = subscriptionsSnapshot.val();
-
-        for (const sub of Object.values(subscriptions)) {
-          const pushConfig = {
-            endpoint: sub.endpoint,
-            keys: {
-              auth: sub.keys.auth,
-              p256dh: sub.keys.p256dh,
-            },
-          };
-
-          try {
-            await webpush.sendNotification(pushConfig, JSON.stringify({
-              title: "New Post",
-              content: "New Post added!",
-              openUrl: "/help",
-            }));
-          } catch (err) {
-            console.log("Error sending notification:", err);
-          }
-        }
-
-        response.status(201).json({ message: "Data stored", id: fields.id });
-      } catch (err) {
-        console.log("Error uploading file or storing data:", err);
-        response.status(500).json({ error: err.message });
+      // -----testing GPT----
+      // Validate input
+      if (!id || !title || !location || !image) {
+        throw new Error('Missing required fields: id, title, location, or image');
       }
-    });
+      // ------------------------finished testing GPT------------
 
-    // End the busboy process
-    busboy.end(request.rawBody);
+
+
+      // Push Data to the Realtime Database
+      await admin.database().ref('posts').push({  // admin -> OfflineAudioCompletionEvent
+        id,
+        title,
+        location,
+        image
+      });
+
+      res.status(201).json({message: "Data Stored", id});
+    }catch (error){
+      console.log('Error storing post:', error.message);  // test GPT
+      res.status(500).json({error: error.message});
+    }
   });
 });
--- public/src/js/utility.js
+++ public/src/js/utility.js
@@ -8,6 +8,7 @@ var dbPromise = idb.open('posts-store', 1, function (db) {
   }
 });
 
+
 function writeData(st, data) {
   return dbPromise
     .then(function(db) {
@@ -48,31 +49,4 @@ function deleteItemFromData(st, id) {
     .then(function() {
       console.log('Item deleted!');
     });
-}
-
-function urlBase64ToUint8Array(base64String) {
-  var padding = '='.repeat((4 - base64String.length % 4) % 4);
-  var base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
-
-  var rawData = window.atob(base64);
-  var outputArray = new Uint8Array(rawData.length);
-
-  for (var i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
-function dataURItoBlob(dataURI) {
-  var byteString = atob(dataURI.split(',')[1]);
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-  var ab = new ArrayBuffer(byteString.length);
-  var ia = new Uint8Array(ab);
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-  var blob = new Blob([ab], {type: mimeString});
-  return blob;
 }
